576382a4fa4761ba8d0ac7320919b9ddd1e2ec5c
Sabrina Wullschleger <sabrina.joerg@ost.ch>
2022-05-10 21:57:50 +0200

    remove host and port info

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -10,14 +10,12 @@ console.log('env', process.env.NODE_ENV);
 export const DatabaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   database: process.env.TYPEORM_DATABASE,
-  port: parseInt(process.env.TYPEORM_PORT),
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
-  host: process.env.TYPEORM_HOST,
   extra: {
     socketPath: process.env.TYPEORM_HOST,
   },
-  synchronize: false,
+  synchronize: true,
   entities: [rootDir + '/**/*.entity{.ts,.js}'],
   autoLoadEntities: true,
 };
