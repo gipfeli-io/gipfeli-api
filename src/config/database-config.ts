9d9f72f5308fc01574d86719292de7c9daffcd4b
Sabrina Wullschleger <sabrina.joerg@ost.ch>
2022-05-12 19:13:23 +0200

    remove socket path and re-add port

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -10,13 +10,11 @@ console.log('env', process.env.NODE_ENV);
 export const DatabaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   database: process.env.TYPEORM_DATABASE,
+  port: parseInt(process.env.TYPEORM_PORT),
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   host: process.env.TYPEORM_HOST,
-  extra: {
-    socketPath: process.env.TYPEORM_HOST,
-  },
-  synchronize: true,
+  synchronize: false,
   entities: [rootDir + '/**/*.entity{.ts,.js}'],
   autoLoadEntities: true,
 };
