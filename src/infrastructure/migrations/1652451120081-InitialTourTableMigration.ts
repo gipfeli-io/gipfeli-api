6ffe5656aeab7697a1f36f104d9be263805f4752
Sabrina Wullschleger <sabrina.joerg@ost.ch>
2022-05-13 16:49:28 +0200

    remove extension. usage not possible because of user rights.

--- src/infrastructure/migrations/1652451120081-InitialTourTableMigration.ts
+++ src/infrastructure/migrations/1652451120081-InitialTourTableMigration.ts
@@ -6,8 +6,6 @@ export class InitialTourTableMigration1652451120081
   name = 'InitialTourTableMigration1652451120081';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS postgis`);
-
     await queryRunner.query(
       `CREATE TABLE "tour" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "startLocation" geometry(Point,4326), "endLocation" geometry(Point,4326), "description" text NOT NULL, "CreatedAt" TIMESTAMP NOT NULL DEFAULT now(), "UpdatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, CONSTRAINT "PK_972cd7fa4ec39286068130fa3f7" PRIMARY KEY ("id"))`,
     );
@@ -33,6 +31,5 @@ export class InitialTourTableMigration1652451120081
       `DROP INDEX "public"."IDX_8ea60097c7fe7d31fb2c26b6e6"`,
     );
     await queryRunner.query(`DROP TABLE "tour"`);
-    await queryRunner.query(`DROP EXTENSION IF EXISTS postgis`);
   }
 }
