11e6c8f80bc4b1317590a8619ee1c6bff7ece0a8
Lukas Merz <lukemerz@gmail.com>
2022-06-14 20:34:06 +0200

    Fix wrong URL

--- src/notification/providers/sendgrid-notification/messages/sign-up.message.ts
+++ src/notification/providers/sendgrid-notification/messages/sign-up.message.ts
@@ -8,7 +8,7 @@ class SignUpMessage {
     const baseUrl = process.env.APP_URL || 'http://localhost:3001';
     return {
       subject: 'Activate your gipfeli.io account!',
-      text: `Hi there!\nYou recently signed up on gipfeli.io.Please use the following link to activate your account:\n\nhttps://staging.gipfeli.io/user/activate/${userId}/${token}\n\nGlad to have you here,\nyour gipfeli.io Team`,
+      text: `Hi there!\nYou recently signed up on gipfeli.io.Please use the following link to activate your account:\n\n${baseUrl}user/activate/${userId}/${token}\n\nGlad to have you here,\nyour gipfeli.io Team`,
       html: `Hi there!<br>You recently signed up on gipfeli.io.<br>Please use the following link to activate your account:<br><br><a href="${baseUrl}user/activate/${userId}/${token}">Activate account<br><br>Glad to have you here,<br>your gipfeli.io Team`,
     };
   }
