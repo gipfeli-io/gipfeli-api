e34cef19e9305841ad77fe152d0f7d1d2a7dde11
Lukas Merz <lukemerz@gmail.com>
2022-07-01 17:38:32 +0200

    Add possibility to use GCP credentials without service account

--- src/config/integrations.config.ts
+++ src/config/integrations.config.ts
@@ -10,10 +10,16 @@ export default registerAs('integrations', () => ({
   },
   googleCloudStorage: {
     bucketName: process.env.GCS_BUCKET,
+    /**
+     * The credentials should contain a base64 encoded key file linked to a service account that has object.create
+     * rights on the specified buckets. Note that in Cloud Run environments, this environment variable does not have to
+     * be set, because the client library takes the currently running cloud user. So only  our Cloud Run service account
+     * needs to have access.
+     */
     credentials: process.env.GCS_SERVICE_ACCOUNT
       ? JSON.parse(
           Buffer.from(process.env.GCS_SERVICE_ACCOUNT, 'base64').toString(),
         )
-      : {},
+      : null,
   },
 }));
