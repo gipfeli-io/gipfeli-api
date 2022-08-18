d4ff7c5a317b43d03ff0d355d62188746f95927a
Sabrina Wullschleger <sabrina.pamela.joerg@gmail.com>
2022-08-18 21:56:56 +0200

    add category dto include category relation when returning tour

--- src/tour/dto/tour.dto.ts
+++ src/tour/dto/tour.dto.ts
@@ -11,6 +11,7 @@ import {
 import { IsPoint } from './validators/is-point.decorator';
 import { SavedImageDto } from '../../media/dto/image.dto';
 import { SavedGpxFileDto } from '../../media/dto/gpx-file.dto';
+import { TourCategoryDto } from './tour-category.dto';
 
 export class TourDto {
   @IsUUID()
@@ -49,17 +50,18 @@ export class TourDto {
 
   @IsOptional()
   gpxFile: SavedGpxFileDto;
+
+  @IsArray()
+  categories: TourCategoryDto[];
 }
 
 export class UpdateTourDto extends PartialType(
   OmitType(TourDto, ['createdAt', 'updatedAt', 'userId']),
-) {
-}
+) {}
 
 export class CreateTourDto extends OmitType(TourDto, [
   'id',
   'createdAt',
   'updatedAt',
   'userId',
-] as const) {
-}
+] as const) {}
