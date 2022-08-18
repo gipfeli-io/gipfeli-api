feb3616bb46c21566b035681732a70cb73a36651
Sabrina Wullschleger <sabrina.pamela.joerg@gmail.com>
2022-08-18 21:47:26 +0200

    add new entity for category add migration file and seed data

---
+++ src/tour/entities/tour-category.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Tour } from './tour.entity';
+
+@Entity()
+export class TourCategory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Tour, (tour) => tour.categories)
+  tours: Tour[];
+}
